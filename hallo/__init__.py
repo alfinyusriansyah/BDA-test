86627c994dbaab6be9e09d65d794cd7d7f07dbd6
alfinyusriansyah <alfinyus001@gmail.com>
2024-01-16 12:01:43 +0700

    deploy app tes bda

--- hallo/__init__.py
+++ hallo/__init__.py
@@ -1,9 +1,14 @@
 import requests
 from flask import Flask, request, jsonify
+import dotenv
+import os
+dotenv.load_dotenv()
+
+PB_URL = os.environ.get("PB_URL")
 
 def name():
     try :
-        res = requests.get("http://10.1.111.7:30809/api/collections/tes_bda/records")
+        res = requests.get(f"{PB_URL}/api/collections/tes_bda/records")
         data = res.json()
         return jsonify(data)
     except Exception as e :
@@ -12,7 +17,7 @@ def name():
 def add_name():
     try :
         body = request.get_json()
-        res = requests.post("http://10.1.111.7:30809/api/collections/tes_bda/records", json=body)
+        res = requests.post(f"{PB_URL}/api/collections/tes_bda/records", json=body)
         data = res.json()
         return jsonify (data), 201
     except Exception as e :
@@ -21,7 +26,7 @@ def add_name():
 def update_name(id):
     try :
         body = request.get_json()
-        res = requests.patch(f"http://10.1.111.7:30809/api/collections/tes_bda/records/{id}", json=body)
+        res = requests.patch(f"{PB_URL}/api/collections/tes_bda/records/{id}", json=body)
         data = res.json()
         status_code = res.status_code
         return jsonify (data), status_code
@@ -30,7 +35,7 @@ def update_name(id):
 
 def delete_name(id):
     try:
-        res = requests.delete(f"http://10.1.111.7:30809/api/collections/tes_bda/records/{id}")
+        res = requests.delete(f"{PB_URL}/api/collections/tes_bda/records/{id}")
         data = res.json()
         status_code = res.status_code
         return jsonify (data), status_code
@@ -39,7 +44,7 @@ def delete_name(id):
 
 def view_name(id):
     try:
-        res = requests.get(f"http://10.1.111.7:30809/api/collections/tes_bda/records/{id}") 
+        res = requests.get(f"{PB_URL}/api/collections/tes_bda/records/{id}") 
         data = res.json()
         status_code = res.status_code
         return jsonify (data), status_code
